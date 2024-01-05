f046261ccc022fa60839792bfd0c9add49f0ed47
Valentin Simioni <valentin.simioni@outlook.com>
2024-01-05 11:31:22 +0100

    Step 3 : Class for tickets, follows created and tested in admin mode

--- LITRevu/applicationLITRevu/models.py
+++ LITRevu/applicationLITRevu/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class CustomUser(AbstractUser):
